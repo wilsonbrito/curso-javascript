3ff051257fb54edc3320ea3957e36657a7ca7ee9
wilsonbrito <wilsonjb93@gmail.com>
2018-05-12 13:37:17 -0300

    sáb, 12 de mai, 13:37

---
+++ fundamentos/comentario.js
@@ -0,0 +1,16 @@
+//Comentário de uma linha
+console.log('exemplo 1')
+
+/*
+    Comentário com mais 
+    de uma linha
+*/
+console.log('exemplo 2')
+
+/*
+* Comentário com mais 
+* de uma linha
+* com formatação diferente
+*/
+console.log('exemplo 3')
+
