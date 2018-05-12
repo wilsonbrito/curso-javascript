054c671affd39eeaf851fec202f4343bd72d4188
wilsonbrito <wilsonjb93@gmail.com>
2018-05-12 15:13:34 -0300

    sab, 12 de mai, 15:13

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+
+//Evitar nomes genéricos
+let valor = ''
+let numero = 1
+let pqp = false //Produto Quimíco Perigoso ... kkkk
+
