c10258e958f3e4b34edab23d8be71972fca71f10
wilsonbrito <wilsonjb93@gmail.com>
2018-05-12 14:15:41 -0300

    sab, 12 de mai, 14:15

--- fundamentos/variaveisEConstantes.js
+++ fundamentos/variaveisEConstantes.js
@@ -1,6 +1,6 @@
 var numeros = 3 //atribuição do valor 3 ao identificador chamado "numeros"
 let numeross = 4 //atribuição do valor 4 ao identificador chamado "numeross"
-const numerosss = 5 //atribuição do valor 5 ao identificador chamado "numerosss"
+const numerosss = 5 //atribuição do valor 5 ao identificador chamado "numerosss" e não posso fazer uma atribuição depois, por ela ser constante.
 
 /*
 * Se eu declarar no let novamente com o mesmo identificador outro valor, irá reportar no output que estou tentando declarar novamente o valor.
@@ -14,3 +14,5 @@ console.log(numeros,numeross, numerosss)
 
 numeros = 300
 numeross = 400
+
+console.log(numeros,numeross, numerosss)
