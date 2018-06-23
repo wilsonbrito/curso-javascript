a83392b219ea92b0c6f6f79b8f742564a6b6ffec
wilsonbrito <wilsonjb93@gmail.com>
2018-06-23 13:54:05 -0300

    Destructuring

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 //abtribuição normal
+let b = 3 //atribuição normal
+
+b += a //atribuição aditiva = b = b + a
+console.log(b)
+
+b -= 4 //b = b - 4
+console.log(b)
+
+b *= 2 //b = b *2
+console.log(b)
+
+b /= 2 //b = b / 2
+console.log(b)
+
+b %= 2 //Atribuição modular b = b % 2
+console.log(b)
