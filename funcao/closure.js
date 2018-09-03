dd82fcb16d41193433bd5dcde2d8de65ae875bdb
wilsonbrito <wilsonjb93@gmail.com>
2018-09-03 14:13:18 -0300

    closure

---
+++ funcao/closure.js
@@ -0,0 +1,16 @@
+//Closure é o escopo criado quando uma função é declarada
+// Esse escopo permite a função acessar e manipular variáveis externas à função
+
+//Contexto Léxico em ação
+const x = 'global'
+
+function fora() {
+    const x = 'local'
+    function dentro(){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
