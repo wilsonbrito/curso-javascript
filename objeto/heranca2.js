38ee0c8d3e0abf7fbdaec3bb5ab990b064210f68
wilsonbrito <wilsonjb93@gmail.com>
2018-10-28 19:36:39 -0300

    herança

---
+++ objeto/heranca2.js
@@ -0,0 +1,44 @@
+//Cadeia de protótipos (prototype chain)
+const avo = { attr1: 'A'}
+const pai = { __proto__: avo, attr2: 'B'}
+const filho = { __proto__: pai, attr3: 'C'}
+console.log(filho.attr1)
+
+//Se caso tenha o mesmo atributo no filho e no pai e você pede para imprimir pelo filho, vai imprimir o valor do filhos,
+// pois está realizando o sombreamento
+
+const carro = {
+    velAtual: 0,
+    valMax: 200,
+    acelerarMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        } else {
+            this.velAtual = this.velMax
+        }
+    },
+    status() {
+        return `${this.velAtual} Km/h de ${this.velMax}Km/h`
+    }
+}
+
+const ferrari = {
+    modelo : 'F40',
+    velMax: 324 //shadowing
+}
+
+const volvo = {
+    modelo: 'V40',
+    status() {
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelerarMais(100)
+console.log(volvo.status())
---
+++ objeto/heranca4.js
@@ -0,0 +1,6 @@
+function MeuObjeto(){}
+console.log(MeuObjeto.prototype)
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+console.log(obj1.__proto__ === obj2.__proto__)
