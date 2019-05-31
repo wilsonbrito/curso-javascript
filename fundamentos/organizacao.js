8d210f0466e434c7836eeb5e34e4933689becb21
wilsonbrito <wilsonjb93@gmail.com>
2019-05-30 21:11:40 -0300

    Novas Aulas

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -1,8 +1,12 @@
-console.log('sentença de código')
+/* Mostrando as formas que da para ser feito a impressão de uma informação*/
+
+
+console.log("sentença de código")
+console.log('sentença de código') //padrão que vamos adotar
 
 {
     {
-        console.log('olá')
+        console.log("olá")
         console.log('mundo')
     }
 }
